50fc63fb5044e7f78e0f30b98b9c3e492e70e224
Easton Man <manyang.me@outlook.com>
2021-03-13 21:43:53 +0800

    chore(test): fix get_account_info_test and edit_page_test

--- get_page_list_test.go
+++ get_page_list_test.go
@@ -8,12 +8,12 @@ import (
 )
 
 func Test_client_GetPageList(t *testing.T) {
-	t.Run("invalid", func(t *testing.T) {
+	t.Run("Invalid", func(t *testing.T) {
 		c := telegraph.NewClient()
 		_, err := c.GetPageList(0, 0)
 		assert.Error(t, err)
 	})
-	t.Run("valid", func(t *testing.T) {
+	t.Run("Valid", func(t *testing.T) {
 		c, err := telegraph.NewClientWithToken("b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb")
 
 		assert.NoError(t, err)
